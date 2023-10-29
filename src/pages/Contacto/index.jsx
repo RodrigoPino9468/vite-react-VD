81f418b26b42a87c80dd40f26b57a928300bbd6a
edgar-ramxs <edgararamosv@gmail.com>
2023-10-29 17:04:47 -0300

    ADD: contactos cambos invalidos

--- src/pages/Contacto/index.jsx
+++ src/pages/Contacto/index.jsx
@@ -1,47 +1,149 @@
+import { useEffect, useState } from "react";
+// https://www.escuelafrontend.com/formularios-en-react
 export default function Index() {
+
+  const [camposInvalidos, setCamposInvalidos] = useState({
+    nombre: false,
+    correo: false,
+    asunto: false,
+    mensaje: false,
+  });
+
+  const verificarDatos = ({ nombre, correo, asunto, mensaje }) => {
+
+  }
+
+  const handleSubmit = (evento) => {
+    evento.preventDefault();
+    const { nombre, correo, asunto, mensaje } = evento.target.elements;
+    const datos = {
+      nombre: nombre.value,
+      correo: correo.value,
+      asunto: asunto.value,
+      mensaje: mensaje.value,
+    };
+    console.log(datos);
+  };
+
   return (
     <main className="container my-5 p-2">
+      {/* Container de la pagina */}
       <div className="row mx-5 mx-sm-3">
-        <aside className="p-2 col-12 col-md-6">
+        {/* Información de contacto */}
+        <section className="p-2 col-12 col-md-6">
           <article>
-            <h4>Teléfono</h4>
-            <h6>+56 9 4993 3983</h6>
+            <h2>Teléfono</h2>
+            <p>+56 9 4993 3983</p>
           </article>
           <article>
-            <h4>Email</h4>
-            <h6>hola@fundacionvidaindependiente.cl</h6>
+            <h2>Email</h2>
+            <p>hola@fundacionvidaindependiente.cl</p>
           </article>
           <article>
-            <h4>Ubicación</h4>
-            <h6>Antonio varas 880, Providencia.</h6>
+            <h2>Ubicación</h2>
+            <p>Antonio varas 880, Providencia.</p>
           </article>
           <article>
-            <h4>Horario</h4>
-            <h6>
+            <h2>Horario</h2>
+            <p>
               Lunes a Viernes:
               <br />
               14:00 - 14:05
-            </h6>
+            </p>
           </article>
-        </aside>
-        <section className="p-3 col-12 col-md-6">
-          <form action="#">
-            <div className="row">
-              <div className="col-6 mb-3">
-                <input type="text" className="form-control" placeholder="Nombre" />
-              </div>
-              <div className="col-6 mb-3">
-                <input type="email" className="form-control" placeholder="Correo electronico" />
-              </div>
-              <div className="col-12 mb-3">
-                <input type="text" className="form-control" placeholder="Asunto" />
-              </div>
-              <div className="col-12 mb-3">
-                <textarea name="" id="" cols="30" rows="7" className="form-control" placeholder="¡Escriba su mensaje que pronto estaremos comunicandonos con usted!"></textarea>
-              </div>
-              <div className="col-12 d-flex justify-content-center">
-                <input type="submit" value="Enviar mensaje" className="btn btn-primary" />
-              </div>
+        </section>
+
+        {/* Formulario de contacto */}
+        <section className="p-2 col-12 col-md-6">
+          <h4 className="mb-3">Formulario de Contacto</h4>
+          <form
+            id="formularioContacto"
+            className="needs-validation"
+            noValidate
+            onSubmit={handleSubmit}
+          >
+            <article className="row g-3">
+              <section className="col-sm-6">
+                <label htmlFor="nombre" className="form-label">
+                  Nombre
+                </label>
+                <input
+                  id="nombre"
+                  type="text"
+                  className="form-control"
+                  name="nombre"
+                  placeholder=""
+                  required
+                  autoComplete="off"
+                />
+                <p className="invalid-feedback">
+                  Valid first name is required.
+                </p>
+              </section>
+
+              <section className="col-sm-6">
+                <label htmlFor="correo" className="form-label">
+                  Correo Electronico
+                </label>
+                <input
+                  id="correo"
+                  type="text"
+                  className="form-control"
+                  name="correo"
+                  placeholder=""
+                  required
+                  autoComplete="off"
+                />
+                <p className="invalid-feedback">
+                  Valid first name is required.
+                </p>
+              </section>
+
+              <section className="col-12">
+                <label htmlFor="asunto" className="form-label">
+                  Asunto
+                </label>
+                <input
+                  id="asunto"
+                  type="text"
+                  className="form-control"
+                  name="asunto"
+                  placeholder=""
+                  required
+                  autoComplete="off"
+                />
+                <p className="invalid-feedback">
+                  Valid first name is required.
+                </p>
+              </section>
+
+              <section className="col-12">
+                <label htmlFor="mensaje" className="form-label">
+                  Mensaje
+                </label>
+                <textarea
+                  id="mensaje"
+                  name="mensaje"
+                  cols="30"
+                  rows="7"
+                  className="form-control"
+                  placeholder="¡Escriba su mensaje que pronto estaremos comunicandonos con usted!"
+                  required
+                  autoComplete="off"
+                ></textarea>
+                <p className="invalid-feedback">
+                  Valid first name is required.
+                </p>
+              </section>
+            </article>
+            <div className="col-12 d-flex justify-content-center">
+              <button
+                name="submitContacto"
+                className="btn btn-primary mt-3"
+                type="submit"
+              >
+                Enviar mensaje
+              </button>
             </div>
           </form>
         </section>
